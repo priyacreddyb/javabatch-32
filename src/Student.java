4cfae3623f17405be645060947408c815615251b
chandu <priyacreddyb@gmail.com>
2023-06-01 16:54:32 -0500

    commiting

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+    private String Name;
+
+    // Getter for name
+    public String getName() {
+        return Name;
+    }
+
+    // Setter for name
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+}
