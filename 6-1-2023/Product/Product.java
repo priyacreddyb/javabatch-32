03bd0a3859b36af79011dd9ba2776ff78fc467eb
chandu <priyacreddyb@gmail.com>
2023-06-02 09:33:22 -0500

    Create a class product with variable price

---
+++ 6-1-2023/Product/Product.java
@@ -0,0 +1,16 @@
+public class Product {
+
+    private double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice(int Quantity){
+      return price;
+    }
+}
