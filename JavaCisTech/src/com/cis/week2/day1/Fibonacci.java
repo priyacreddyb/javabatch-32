0d069a1d4ee8fac8c807c2bfcbe2d8164de5d5b2
chandu <priyacreddyb@gmail.com>
2023-06-06 10:47:54 -0500

    Fibonacci

--- JavaCisTech/src/com/cis/week2/day1/Fibonacci.java
+++ JavaCisTech/src/com/cis/week2/day1/Fibonacci.java
@@ -1,2 +1,23 @@
-package com.cis.week2.day1;public class Fibonacci {
+package com.cis.week2.day1;
+
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Number:");
+        int n=sc.nextInt();
+        int first=0;
+        int second=1;
+        int last;
+        System.out.print(first+"  "+second);
+        for(int i=2;i<=n;i++){
+             last=first+second;
+             first=second;
+             second=last;
+            System.out.print(" "+last);
+
+        }
+    }
 }
