e849d37e580bf6d062bde16d2a39f9678706c863
chandu <priyacreddyb@gmail.com>
2023-06-06 10:48:54 -0500

    PersonAge

--- JavaCisTech/src/com/cis/week2/day1/PersonAge.java
+++ JavaCisTech/src/com/cis/week2/day1/PersonAge.java
@@ -1,2 +1,21 @@
-package com.cis.week2.day1;public class PersonAge {
+package com.cis.week2.day1;
+
+import java.util.Scanner;
+
+public class PersonAge {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the person age:");
+        int age = sc.nextInt();
+
+        if (age >= 13 && age <= 19) {
+            System.out.println("teen");;
+        } else if (age < 13) {
+            System.out.println("kid");;
+        } else if (age > 19) {
+            System.out.println("adult");;
+        }
+
+    }
 }
