3f45c7f499d27b8d8a1e82c1dd97adcacbd3e3d3
chandu <priyacreddyb@gmail.com>
2023-06-07 10:29:14 -0500

    Rotate an array

--- JavaCisTech/src/com/cis/week2/day2/Rotate.java
+++ JavaCisTech/src/com/cis/week2/day2/Rotate.java
@@ -1,2 +1,17 @@
-package com.cis.week2.day2;public class Rotate {
+package com.cis.week2.day2;
+
+public class Rotate {
+    public static void main(String[] args) {
+        int[] y={2,3,6,1,46,24,87};
+        int N=y.length;
+        int shift=2;
+        for(int i=0;i<N;i++) {
+            if (i < N - shift) {
+                System.out.println(y[i + shift]);
+            }
+        }
+        for(int i=0;i<shift;i++){
+            System.out.println(y[i]);
+        }
+    }
 }
