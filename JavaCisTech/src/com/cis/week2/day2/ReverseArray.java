1e39a4ec1a3de8a8b9f5a9e8fd2e93dc5e2e5e7d
chandu <priyacreddyb@gmail.com>
2023-06-07 10:27:16 -0500

    Reverse of an array

--- JavaCisTech/src/com/cis/week2/day2/ReverseArray.java
+++ JavaCisTech/src/com/cis/week2/day2/ReverseArray.java
@@ -1,2 +1,11 @@
-package com.cis.week2.day2;public class ReverseArray {
+package com.cis.week2.day2;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int [] a={12,33,2,4,55,6667,443};
+
+        for(int i =6;i>=0; --i){
+            System.out.println(a[i]);
+        }
+    }
 }
