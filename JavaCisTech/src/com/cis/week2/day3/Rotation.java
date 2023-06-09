29b059a5224a67aabe1d4e94e9ff9272ead83d67
chandu <priyacreddyb@gmail.com>
2023-06-09 09:22:18 -0500

    Rotation of an array

--- JavaCisTech/src/com/cis/week2/day3/Rotation.java
+++ JavaCisTech/src/com/cis/week2/day3/Rotation.java
@@ -1,2 +1,18 @@
-package com.cis.week2.day3;public class Rotation {
+package com.cis.week2.day3;
+
+public class Rotation {
+    public static void main(String[] args) {
+        String s1="J2eeSpringHibernateDatajpa";
+        String s2="HibernateDatajpaJ2eeSpring";
+        if(s1.length()!=s2.length()){
+            System.out.println("Not Rotation Strings of each other");
+        }
+        s1=s1.concat(s2);
+        if(s1.contains(s2)){
+            System.out.println("These two Rotational strings");
+
+        }
+
+
+    }
 }
