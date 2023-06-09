6664c752e327fa537fe2f9f9803e707a29d7993c
chandu <priyacreddyb@gmail.com>
2023-06-09 09:20:15 -0500

    Reverse each word of a String

--- JavaCisTech/src/com/cis/week2/day3/ReverseEach.java
+++ JavaCisTech/src/com/cis/week2/day3/ReverseEach.java
@@ -1,2 +1,18 @@
-package com.cis.week2.day3;public class ReverseEach {
+package com.cis.week2.day3;
+public class ReverseEach {
+    public static void main(String[] args) {
+        String originalString="Sun Rises in the East";
+        String[] words=originalString.split(" ");
+        String reverse="";
+        for(String s:words){
+            String reversewords="";
+            for(int i= s.length()-1;i>=0;i--){
+                reversewords=reversewords+s.charAt(i);
+            }
+            reverse=reverse+reversewords+" ";
+        }
+        System.out.println(reverse);
+
+
+    }
 }
