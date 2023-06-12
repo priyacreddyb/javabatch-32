faa8f81080a76cd546e5b1ad4796982c75061902
chandu <priyacreddyb@gmail.com>
2023-06-12 12:49:24 -0500

    Adding StudentData.java file

--- JavaCisTech/src/com/cis/week2/day4/StudentData.java
+++ JavaCisTech/src/com/cis/week2/day4/StudentData.java
@@ -1,2 +1,22 @@
-package com.cis.week2.day4;public class StudentData {
+package com.cis.week2.day4;
+
+public class StudentData implements Comparable<StudentData> {
+
+    public String firstName;
+    public String lastName;
+    public double gpa;
+
+    public StudentData(String firstName, String lastName, double gpa) {
+        this.firstName = firstName;
+        this.lastName=lastName;
+        this.gpa=gpa;
+    }
+
+    @Override
+    public int compareTo(StudentData o) {
+        return this.lastName.compareTo(o.lastName);
+    }
+    public void display(){
+        System.out.println(firstName+","+lastName+","+gpa);
+    }
 }
