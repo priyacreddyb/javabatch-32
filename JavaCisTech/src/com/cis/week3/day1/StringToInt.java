7a6c7d8f0efc03c7c357540a850212465c42b16a
chandu <priyacreddyb@gmail.com>
2023-06-13 10:45:22 -0500

    Converting String to Integer

--- JavaCisTech/src/com/cis/week3/day1/StringToInt.java
+++ JavaCisTech/src/com/cis/week3/day1/StringToInt.java
@@ -1,2 +1,16 @@
-package com.cis.week3.day1;public class StringToInt {
+package com.cis.week3.day1;
+
+public class StringToInt {
+    public static void main(String[] args) {
+        String[] sNumber={"23","45.67","test","123f"};
+        int iNumber;
+        try {
+            for(String s:sNumber){
+            iNumber= Integer.parseInt(s);
+            System.out.println(iNumber);
+            }
+        }catch (NumberFormatException e){
+            System.out.println("Given String Cannot be converted as an Integer");
+        }
+    }
 }
