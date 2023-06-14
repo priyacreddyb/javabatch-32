5841d44e3ffc449f248489db6833a3826adb3bb0
chandu <priyacreddyb@gmail.com>
2023-06-14 11:27:48 -0500

    Multithreading by Extending Thread

--- JavaCisTech/src/com/cis/week3/day2/UsingThread.java
+++ JavaCisTech/src/com/cis/week3/day2/UsingThread.java
@@ -1,2 +1,16 @@
-package com.cis.week3.day2;public class UsingThread {
+package com.cis.week3.day2;
+
+class Thread1 extends Thread{
+    public void run(){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
+public class UsingThread {
+    public static void main(String[] args) {
+        Thread1 r=new Thread1();
+        Thread t=new Thread(r);
+        t.start();
+    }
 }
